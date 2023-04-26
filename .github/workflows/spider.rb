e1e7f6a6ec7b20b22f8eb976d525582162eaced5
ei0ch <823085664@qq.com>
2023-04-26 17:31:04 +0800

    Update spider.rb

--- .github/workflows/spider.rb
+++ .github/workflows/spider.rb
@@ -7,32 +7,15 @@ link_path = File.join(__dir__, 'link.txt')
 
 urls = File.readlines(link_path).map(&:strip)
 
+# 爬取链接中的文本
 texts = urls.map do |url|
   doc = Nokogiri::HTML(open(url))
   doc.text
 end
 
-date = Time.now.strftime('%Y-%m-%d')
-time_ = Time.now.strftime('%H:%M:%S')
-num_urls = urls.size
+# 合并文本
+merged_text = texts.join("\n\n")
 
-header = "爬取日期：#{date}，爬取时间：#{time_}，爬取链接数：#{num_urls}\n\n"
-text = header + texts.join("\n\n")
-
-File.write('merged_text.txt', text)
-
-start_time = Time.now
-
-# 爬取的代码
-
-end_time = Time.now
-elapsed_time = end_time - start_time
-
-info = {
-  '爬取日期': date,
-  '爬取时间': time_,
-  '爬取链接数': num_urls,
-  '访问时间': elapsed_time
-}
-
-File.write('tvconfig.json', JSON.generate(info))
+# 将文本转换为 JSON 格式并写入文件
+json_data = { tvconfig: merged_text }.to_json
+File.write('tvconfig.json', json_data)
