f2a57dc940bb4d43ca2e6cc8d7eedc5e38b2c0e5
ei0ch <823085664@qq.com>
2023-04-26 17:36:47 +0800

    Update spider.rb

--- .github/workflows/spider.rb
+++ .github/workflows/spider.rb
@@ -1,16 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
+require 'json'
 
 # 读取链接列表
 urls = File.readlines('link.txt').map(&:strip)
 
 # 爬取链接中的文本
 texts = urls.map do |url|
-  uri = URI.parse(url)
-  doc = Nokogiri::HTML(open(uri))
+  doc = Nokogiri::HTML(open(url))
   doc.text
 end
 
-# 合并文本并写入文件
-merged_text = texts.join("\n\n")
-File.write('merged_text.txt', merged_text)
+# 将文本转换为 JSON 格式并写入文件
+json_data = { merged_text: merged_text }.to_json
+File.write('merged_text.json', json_data)
+
